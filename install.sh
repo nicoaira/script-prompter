126736d5dbafa24dc379040881bade85ceadbb10
Nicolas <aira.nicolas94@gmail.com>
2025-02-10 12:15:15 +0100

    fixed underscore

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ echo "Starting installation of Script Prompter..."
 # ...
 
 # Define repository URL and installation directory
-REPO_URL="https://github.com/nicoaira/script_prompter.git"
+REPO_URL="https://github.com/nicoaira/script-prompter.git"
 INSTALL_DIR="$HOME/.local/script_prompter"
 
 # Clone or update the repository
